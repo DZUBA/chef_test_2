02f10528a101c21953c518bf4151cb2374750df9
DZUBA <zzzdzuba@gmail.com>
2016-11-11 18:12:03 +0200

    Adding depend, mysql setup, start and delete test databases

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,19 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+include_recipe 'yum::default'
+
+# install mysql
+package 'mysql-server' do
+  action :install
+end
+
+# start mysql server
+service 'mysqld' do
+  action :start
+end
+
+execute 'delete-test-db' do
+  command 'mysql -uroot -e "show databases" | grep -v Database | grep -v mysql| grep -v information_schema | gawk \'{print "drop database " $1 ";"}\' | mysql -uroot > /dev/null'
+end
